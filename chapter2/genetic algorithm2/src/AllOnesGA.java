69c780a39c0064f265212cbc14e424eb5157638c
Tianqiang Xu <2286455782@qq.com>
2019-01-25 00:26:59 +0800

    ?

---
+++ chapter2/genetic algorithm2/src/AllOnesGA.java
@@ -0,0 +1,24 @@
+public class AllOnesGA {
+    public static void main(String[] args){
+        GeneticAlgorithm ga = new GeneticAlgorithm(100,0.01, 0.95, 2);
+
+        Population population = ga.initPopulation(50);
+
+        ga.evalPopulation(population);
+        int generation = 1;
+
+        while(ga.isTerminationConditionMet(population) == false){
+            System.out.println("Best solution: " + population.getFittest(0).toString());
+            //crossover
+            population = ga.crossoverPopulation(population);
+            //mutation
+            population = ga.mutatePopulation(population);
+            ga.evalPopulation(population);
+
+            generation++;
+        }
+
+        System.out.println("Found solution in " + generation + "generations");
+        System.out.println("Best solution: " + population.getFittest(0).toString());
+    }
+}

d47b43f41b6467f32eeaca5f5588d79875c284fe
Tianqiang Xu <2286455782@qq.com>
2019-01-26 00:40:57 +0800

    p109

---
+++ chapter4/src/chapter4/GeneticAlgorithm.java
@@ -0,0 +1,38 @@
+package chapter4;
+
+public class GeneticAlgorithm {
+    private int populationSize;
+    private double mutationRate;
+    private double crossoverRate;
+    private int elitismCount;
+    protected int tournamentSize;
+
+    public GeneticAlgorithm(int populationSize, double mutationRate, double crossoverRate, int elitismCount, int tournamentSize){
+        this.populationSize = populationSize;
+        this.mutationRate = mutationRate;
+        this.crossoverRate = crossoverRate;
+        this.elitismCount = elitismCount;
+        this.tournamentSize = tournamentSize;
+    }
+
+    public Population initPopulation(int chromosomeLength){
+        Population population = new Population(this.populationSize, chromosomeLength);
+        return population;
+    }
+
+
+    public boolean isTerminationConditionMet(int generationsCount, int maxGenerations){
+        return (generationsCount > maxGenerations);
+    }
+
+    public Individual selectParent(Population population){
+        Population tournament = new Population(this.tournamentSize);
+        population.shuffle();
+        for(int i = 0; i < this.tournamentSize; i++){
+            Individual tournamentIndividual = population.getIndividual(i);
+            tournament.setIndividual(i, tournamentIndividual);
+        }
+        return tournament.getFittest(0);
+    }
+
+}
---
+++ chapter4/src/chapter4/Individual.java
@@ -0,0 +1,42 @@
+package chapter4;
+
+public class Individual {
+    private int[] chromosome;
+    private double fitness = -1;
+
+    public Individual(int[] chromosome) {
+        this.chromosome = chromosome;
+    }
+
+    public int[] getChromosome() {
+        return this.chromosome;
+    }
+
+    public int getChromosomeLength() {
+        return this.chromosome.length;
+    }
+
+    public void setGene(int offset, int gene) {
+        this.chromosome[offset] = gene;
+    }
+
+    public int getGene(int offset) {
+        return this.chromosome[offset];
+    }
+
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+
+    public double getFitness() {
+        return this.fitness;
+    }
+
+    public String toString() {
+        String output = "";
+        for (int gene = 0; gene < this.chromosome.length; gene++) {
+            output += this.chromosome[gene];
+        }
+        return output;
+    }
+}
